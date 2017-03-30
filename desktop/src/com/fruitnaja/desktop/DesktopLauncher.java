3931f42e0bb213c079a1eb3f0c852a4642e6d634
Unknown <sarunyu.you@gmail.com>
2017-03-30 16:43:01 +0700

    Init GDX

---
+++ desktop/src/com/fruitnaja/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fruitnaja.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fruitnaja.Fruitnaja;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Fruitnaja(), config);
+	}
+}
