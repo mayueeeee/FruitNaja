a22491ac28bd16c2f28f8fda49d2dc7df87971bc
Unknown <sarunyu.you@gmail.com>
2017-04-06 11:19:51 +0700

    Add require class

---
+++ core/src/com/fruitnaja/Music.java
@@ -0,0 +1,7 @@
+package com.fruitnaja;
+
+/**
+ * Created by nutno on 5/4/2560.
+ */
+public class Music {
+}
---
+++ core/src/com/fruitnaja/SocketClient.java
@@ -0,0 +1,7 @@
+package com.fruitnaja;
+
+/**
+ * Created by nutno on 6/4/2560.
+ */
+public class SocketClient {
+}
