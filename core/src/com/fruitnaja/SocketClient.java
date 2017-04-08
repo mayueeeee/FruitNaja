a48aecf8fa6450651e704be8e632b5249a1e6269
Unknown <sarunyu.you@gmail.com>
2017-04-08 17:52:33 +0700

    Try to send data to server

--- core/src/com/fruitnaja/SocketClient.java
+++ core/src/com/fruitnaja/SocketClient.java
@@ -1,7 +1,43 @@
 package com.fruitnaja;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by nutno on 6/4/2560.
  */
 public class SocketClient {
+    private Socket socket;
+
+    public SocketClient(final String ip){
+       Thread client = new Thread(){
+           public void run() {
+               try{
+                   socket = new Socket(ip, 1025);
+                   System.out.println("Socket connected to "+ ip);
+                   DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+                   // Send first message
+                   dOut.writeByte(1);
+                   dOut.writeUTF("This is the first type of message.");
+                   dOut.flush(); // Send off the data
+
+               }catch (Exception e){
+                   System.out.println("Can't register new client");
+                   //e.printStackTrace();
+               }
+
+           }
+       };
+       client.start();
+
+    }
+
+    public void updateScore(int score){
+
+    }
+
+    public void updateStatus(){
+
+    }
 }
--- core/src/com/fruitnaja/SocketServer.java
+++ core/src/com/fruitnaja/SocketServer.java
@@ -1,7 +1,69 @@
 package com.fruitnaja;
 
+import java.io.DataInputStream;
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+
 /**
  * Created by nutno on 6/4/2560.
  */
 public class SocketServer {
+
+    public SocketServer()  {
+        //Start socket server
+        Thread network = new Thread(){
+            public void run(){
+                //System.out.println("Thread Running");
+                final int PORT = 1025;
+                try {
+                    System.out.println("Socket server is running at "+ Inet4Address.getLocalHost().getHostAddress()+":"+PORT);
+                    ServerSocket listener = new ServerSocket(PORT);
+                    //listener.accept();
+                    Socket socket = listener.accept();
+
+                    DataInputStream dIn = new DataInputStream(socket.getInputStream());
+
+                    boolean done = false;
+                    while(!done) {
+                        byte messageType = dIn.readByte();
+
+                        switch(messageType)
+                        {
+                            case 1: // Type A
+                                System.out.println("Message A: " + dIn.readUTF());
+                                break;
+                            case 2: // Type B
+                                System.out.println("Message B: " + dIn.readUTF());
+                                break;
+                            case 3: // Type C
+                                System.out.println("Message C [1]: " + dIn.readUTF());
+                                System.out.println("Message C [2]: " + dIn.readUTF());
+                                break;
+                            default:
+                                done = true;
+                        }
+                    }
+
+                    dIn.close();
+
+
+
+
+
+
+                }
+                catch (Exception e){
+                    System.out.println("Socket server can't start");
+                }
+            }
+        };
+        network.start();
+
+
+
+    }
 }
