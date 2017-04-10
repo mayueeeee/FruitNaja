1b25265661f74855d428c9845f01e04029271f23
Unknown <59011299@kmitl.ac.th>
2017-04-10 20:07:00 +0700

    CAMERA

--- core/src/com/fruitnaja/Fruitnaja.java
+++ core/src/com/fruitnaja/Fruitnaja.java
@@ -3,9 +3,12 @@ package com.fruitnaja;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Fruitnaja extends ApplicationAdapter implements ApplicationListener {
 	SpriteBatch batch;
@@ -13,11 +16,18 @@ public class Fruitnaja extends ApplicationAdapter implements ApplicationListener
 	Texture imgB;
 	int x = 0,y = 0;
 	Person heal = new Charactor("U",200,100,0,1);
-
+	private OrthographicCamera cam;
+	private float rotationSpeed;
 	
 	@Override
 	public void create () {
+		rotationSpeed = 0.5f;
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
+		cam = new OrthographicCamera(1024, 1024 * (h / w));
+		cam.position.set(cam.viewportWidth / 0f, cam.viewportHeight / 0f, 0);
+		cam.update();
 		batch = new SpriteBatch();
 		img = new Texture("chai.png");
 		imgB = new Texture("map/bg-glass.jpg");
@@ -25,18 +35,15 @@ public class Fruitnaja extends ApplicationAdapter implements ApplicationListener
 
 	}
 
-	@Override
-	public void resize(int width, int height) {
-
-	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		handleInput();
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);;
 		System.out.println(heal.getPos().x);
-		y-=200*Gdx.graphics.getDeltaTime();
 		batch.begin();
 		batch.draw(imgB,0,0);
 		batch.draw(img, heal.getPos().x,heal.getPos().y );
@@ -47,10 +54,61 @@ public class Fruitnaja extends ApplicationAdapter implements ApplicationListener
 
 
 	}
-	
+
+	private void handleInput() {
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			cam.zoom += 0.02;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+			cam.zoom -= 0.02;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			cam.translate(-3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			cam.translate(3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			cam.translate(0, -3, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			cam.translate(0, 3, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			cam.rotate(-rotationSpeed, 0, 0, 1);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+			cam.rotate(rotationSpeed, 0, 0, 1);
+		}
+
+		cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+
+		float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+		float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+		cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+		cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		cam.viewportWidth = 30f;
+		cam.viewportHeight = 30f * height/width;
+		cam.update();
+	}
+
+	@Override
+	public void resume() {
+	}
+
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void pause() {
 	}
 }
