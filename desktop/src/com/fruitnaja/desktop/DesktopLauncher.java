ad756525226eeb194df577fc2437cfbe7b821eb6
Sarunyu Chankong <sarunyu.you@gmail.com>
2017-04-10 16:02:57 +0700

    Add thread

--- desktop/src/com/fruitnaja/desktop/DesktopLauncher.java
+++ desktop/src/com/fruitnaja/desktop/DesktopLauncher.java
@@ -1,11 +1,7 @@
 package com.fruitnaja.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.fruitnaja.Fruitnaja;
+import com.fruitnaja.*;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,19 +10,52 @@ import javafx.stage.Stage;
 public class DesktopLauncher extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 300, 275));
-		primaryStage.show();
 
-		class LaunchGDX implements EventHandler<ActionEvent>{
+		Parent root = FXMLLoader.load(getClass().getResource("layouts/main.fxml"));
+		primaryStage.setTitle("FruitNaJa");
+		primaryStage.setScene(new Scene(root, Configure.WIDTH, Configure.HEIGH));
+		//primaryStage.setResizable(false);
+		primaryStage.show();
+		//System.out.println(Inet4Address.getLocalHost().getHostAddress());
+		Thread music = new Thread(){
+			public void run(){
+				Music.play();
+				System.out.println("Music Thread");
+			}
+		};
+		music.start();
+
+		Thread client = new Thread(){
+			public void run(){
+				//Music.playEDM();
+
+				//SocketClient client = new SocketClient("192.168.1.216");
+				System.out.println("Client Thread");
+			}
+		};
+
+
+
+		SocketServer server = new SocketServer();
+
+		//System.out.println("uuu");
+
+
+
+
+
+		/*class LaunchGDX implements EventHandler<ActionEvent>{
 			@Override // Override the handle method
 			public void handle(ActionEvent e) {
+				Configure.loadConfigure();
 				//System.out.println("xxx");
 				LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+				config.forceExit = false;
+				config.width = Configure.WIDTH;
+				config.height = Configure.HEIGH;
 				new LwjglApplication(new Fruitnaja(), config);
 			}
-		}
+		}*/
 
 	}
 	public static void main (String[] arg) {
